c1176dcb07ae3ee11e64a82d66a8a085be96443f
kirnehv <henrik1521@gmail.com>
2021-02-09 13:31:10 -0800

    Testing.

--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -1,5 +1,6 @@
 <?php
 if(isset($_POST['loginButton'])){
+  debug_to_console("About to check");
   //Login button was pressed
   $username = $_POST['loginUsername'];
   $password = $_POST['loginPassword'];
@@ -14,7 +15,6 @@ if(isset($_POST['loginButton'])){
 
   //call function to check is username or password exist
   $result = $account->login($username, $password);
-  debug_to_console("About to check");
   if($result == true){
     debug_to_console("Were in");
     //create a session variable to keep track of logged in user throughout pages
